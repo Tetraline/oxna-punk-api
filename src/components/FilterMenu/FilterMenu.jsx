d4d04e3feb61f261e9869462cdd8443c19a59b92
Tetraline <danielsamways@gmail.com>
2022-07-24 19:50:16 +0100

    add filter menu

--- src/components/FilterMenu/FilterMenu.jsx
+++ src/components/FilterMenu/FilterMenu.jsx
@@ -1,6 +1,22 @@
 import "./FilterMenu.scss";
+import { useState } from "react";
+
 const FilterMenu = () => {
-  return <button>Filter</button>;
+  const [showMenu, setShowMenu] = useState(false);
+  const handleTogglePress = () => setShowMenu(!showMenu);
+
+  return (
+    <>
+      <button onClick={handleTogglePress}>Filter</button>
+      {showMenu && (
+        <select name="abv">
+          <option value="Any">Any</option>
+          <option value="high">High</option>
+          <option value="low">Low</option>
+        </select>
+      )}
+    </>
+  );
 };
 
 export default FilterMenu;
